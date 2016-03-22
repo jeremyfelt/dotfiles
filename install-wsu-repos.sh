5b60c302d2c7c4ff51a8e8740462bc6290103460
Jeremy Felt <jeremy.felt@gmail.com>
2016-03-22 14:14:47 -0700

    Initial setup of various common WSU plugin and theme repositories

---
+++ install-wsu-repos.sh
@@ -0,0 +1,17 @@
+cd ~/Development/wsuwp-platform/build-plugins/
+git clone git@github.com:washingtonstateuniversity/WSUWP-Build-Plugins-Public.git public
+git clone git@github.com:washingtonstateuniversity/WSUWP-Build-Plugins-Private.git private
+cd ~/Development/wsuwp-platform/build-themes/
+git clone git@github.com:washingtonstateuniversity/WSUWP-Build-Themes-Public.git public
+
+cd ~/Development/wsuwp-platform/www/wp-content/plugins/
+git clone git@github.com:washingtonstateuniversity/WSUWP-Content-Syndicate.git wsuwp-content-syndicate
+git clone git@github.com:washingtonstateuniversity/WSUWP-Content-Visibility.git wsuwp-content-visibility
+git clone git@github.com:washingtonstateuniversity/WSUWP-Admin.git wsuwp-admin
+git clone git@github.com:washingtonstateuniversity/WSUWP-Plugin-Custom-CSS.git wsuwp-custom-css
+git clone git@github.com:washingtonstateuniversity/WSUWP-Plugin-SSO-Authentication.git wsuwp-sso-authentication
+git clone git@github.com:washingtonstateuniversity/WSU-University-Taxonomy.git wsuwp-university-taxonomy
+git clone git@github.com:washingtonstateuniversity/WSUWP-Plugin-Embeds.git wsuwp-embeds
+
+cd ~/Development/wsuwp-platform/www/wp-content/themes/
+git clone git@github.com:washingtonstateuniversity/wsu.edu.git wsu.edu

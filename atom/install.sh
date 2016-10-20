07b1cb4e11fbfd1a879842dfaf70f071fae2812c
Jeremy Felt <jeremy.felt@gmail.com>
2016-10-20 10:09:58 -0700

    Install atom text editor via deb file

--- atom/install.sh
+++ atom/install.sh
@@ -2,7 +2,10 @@
 #
 # Atom
 #
-# Install apm packages used by Atom.
+# Install or update Atom with the latest debian package.
+cd /tmp
+wget https://atom.io/download/deb -O atom.deb
+sudo dpkg -i atom.deb
 
 # Install a monokai theme for Atom.
 apm install atom-monokai

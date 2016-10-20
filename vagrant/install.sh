ada06ffdc8cd852acc9614806136a04b29de1a11
Jeremy Felt <jeremy.felt@gmail.com>
2016-10-20 10:10:32 -0700

    Install Vagrant automatically via .deb file

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -2,7 +2,9 @@
 #
 # Vagrant
 #
-# This installs any plugins or other related packages for Vagrant.
+cd /tmp
+wget https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_x86_64.deb -O vagrant.deb
+sudo dpkg -i vagrant.deb
 
 vagrant plugin install vagrant-hostsupdater
 vagrant plugin install vagrant-hosts

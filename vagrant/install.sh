3c1682a254db949c42476018a840181f5910ed60
Jeremy Felt <jeremy.felt@gmail.com>
2016-12-28 18:10:18 -0800

    Install Vagrant 1.9.1

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -3,7 +3,7 @@
 # Vagrant
 #
 cd /tmp
-wget https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_x86_64.deb -O vagrant.deb
+wget https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.deb -O vagrant.deb
 sudo dpkg -i vagrant.deb
 
 vagrant plugin install vagrant-hostsupdater

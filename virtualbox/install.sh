b84d55e2c493548db4861ec2252848cf8bed1d6b
Jeremy Felt <jeremy.felt@gmail.com>
2016-12-28 17:51:11 -0800

    Add VirtualBox to the list of manual applications to install

--- virtualbox/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Vagrant
-#
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" > /etc/apt/sources.list.d/virtualbox.list'
-
-sudo apt-get update
-sudo apt-get install -y virtualbox-5.1 dkms

499d867f545d7bfff0ebf44adde8ba4643961a49
Jeremy Felt <jeremy.felt@gmail.com>
2018-04-25 12:30:03 -0700

    Update to PHP7.2 using latest brew config

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,2 @@
 export PATH="./bin:$HOME/bin:$HOME/.composer/vendor/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH="$(brew --prefix homebrew/php/php71)/bin:$PATH"

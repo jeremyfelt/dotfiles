214c1ab868ce640688997293b8151776395e37c3
Jeremy Felt <jeremy.felt@gmail.com>
2018-06-02 14:35:23 -0700

    Update WP-CLI to the latest version

--- wp-cli/install.sh
+++ wp-cli/install.sh
@@ -7,4 +7,6 @@ then
 	curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /tmp/wp-cli.phar
 	chmod +x /tmp/wp-cli.phar
 	mv /tmp/wp-cli.phar /usr/local/bin/wp
+else
+	wp cli update --yes
 fi

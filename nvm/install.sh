90bd39a093c8311483e59b89fe579d19f8d240bc
Jeremy Felt <jeremy.felt@gmail.com>
2018-06-02 14:03:28 -0700

    Check for the proper version of NVM before reinstalling
    This upgrades NVM to 0.33.11 as well.

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 #
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+export NVM_DIR=$HOME/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+if test ! $(nvm --version | grep "33.11")
+then
+  rm -rf $HOME/.nvm
+  mkdir $HOME/.nvm
+  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+fi

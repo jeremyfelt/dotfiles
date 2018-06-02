e2ba066ba889ab43600488911d1b338533b6b287
Jeremy Felt <jeremy.felt@gmail.com>
2018-06-02 14:33:55 -0700

    Better manage global node and npm packages

--- node/install.sh
+++ node/install.sh
@@ -10,19 +10,14 @@ then
   wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 fi
 
-if test ! $(which spoof)
-then
-  # Install spoof to enable MAC address spoofing.
-  sudo npm install spoof -g
-fi
+nvm install stable
+nvm use stable
 
-if test ! $(which grunt-cli)
-then
-  # Install grunt as a global npm package.
-  npm install -g grunt-cli
-fi
+# Install grunt as a global npm package.
+npm install -g grunt-cli
 
-if test ! $(which npm-check-updates)
-then
-  npm install -g npm-check-updates
-fi
+# Help check for package updates in projects.
+npm install -g npm-check-updates
+
+# Install spoof to enable MAC address spoofing.
+sudo npm install spoof -g

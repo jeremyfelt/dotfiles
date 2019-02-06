29089e90ebd7e68f6170cb7f0ce30ca924edf1ad
Jeremy Felt <jeremy.felt@gmail.com>
2019-02-05 17:43:18 -0800

    Adding php7.2 to my path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
 export PATH="./bin:$HOME/bin:$HOME/.composer/vendor/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="/usr/local/opt/php@7.2/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

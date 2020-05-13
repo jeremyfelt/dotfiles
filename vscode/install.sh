d6ef21584f11d96d169f8d8625f8c481555b8750
Jeremy Felt <jeremy.felt@gmail.com>
2020-05-12 18:20:55 -0700

    Add symlinked keybindings; override terminal pane switching

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,5 +1,8 @@
 rm $HOME/Library/Application\ Support/Code/User/settings.json
 ln -s $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
-rm $HOME/Library/Application\ Support/Code/User/snippets/
-ln -s $HOME/dotfiles/vscode/snippets/ $HOME/Library/Application\ Support/Code/User/snippets
+rm $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -s $HOME/dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+
+rm -rf $HOME/Library/Application\ Support/Code/User/snippets/
+ln -s $HOME/dotfiles/vscode/snippets $HOME/Library/Application\ Support/Code/User/snippets

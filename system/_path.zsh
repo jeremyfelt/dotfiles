72d6c82fa1a0a64815874999e873000bcde3d55c
jeremyfelt <jeremy.felt@gmail.com>
2022-01-07 12:30:44 -0800

    Fix some path locations

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,6 @@
-export PATH="./bin:$HOME/bin:$HOME/.composer/vendor/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export PATH="/usr/local/opt/php@7.2/bin:$PATH"
+export PATH=""
+export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$ZSH/bin"
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+export PATH="$PATH:/usr/local/opt/php@7.4/bin"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

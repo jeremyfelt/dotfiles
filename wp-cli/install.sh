81bf68f112449aececa65c631dfdc5543ad2aef8
Jeremy Felt <jeremy.felt@gmail.com>
2022-02-05 10:52:46 -0800

    Use sudo to mv wp-cli into the bin directory?

--- wp-cli/install.sh
+++ wp-cli/install.sh
@@ -6,7 +6,7 @@ if test ! $(which wp)
 then
 	curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /tmp/wp-cli.phar
 	chmod +x /tmp/wp-cli.phar
-	mv /tmp/wp-cli.phar /usr/local/bin/wp
+	sudo mv /tmp/wp-cli.phar /usr/local/bin/wp
 else
 	wp cli update --yes
 fi

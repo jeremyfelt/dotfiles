a3d0c66efbba2d084922c393162cb81bd0ad9cf9
Jeremy Felt <jeremy.felt@gmail.com>
2022-02-21 12:00:52 -0800

    Remove older path definitions

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,6 +1,1 @@
-export PATH=""
-export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$ZSH/bin"
-export PATH="$PATH:$HOME/.composer/vendor/bin"
-export PATH="$PATH:/usr/local/opt/php@7.4/bin"
-
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

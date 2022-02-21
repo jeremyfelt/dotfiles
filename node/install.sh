956f275ff700fb2c829cfef6a6ea377d2a086a71
Jeremy Felt <jeremy.felt@gmail.com>
2022-02-21 11:15:16 -0800

    Improve initial NVM installation and configuration

--- node/install.sh
+++ node/install.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 #
-export NVM_DIR=$HOME/.nvm
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# NVM: https://github.com/nvm-sh/nvm
 
-if test ! $(nvm --version | grep "33.11")
+if test ! $(nvm --version | grep "39.1")
 then
   rm -rf $HOME/.nvm
   mkdir $HOME/.nvm
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 fi
 
-nvm install stable
-nvm use stable
+export NVM_DIR=$HOME/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-# Install grunt as a global npm package.
-npm install -g grunt-cli
+nvm install --lts --latest-npm
+nvm use --lts
 
 # Help check for package updates in projects.
 npm install -g npm-check-updates
-
-# Install spoof to enable MAC address spoofing.
-sudo npm install spoof -g

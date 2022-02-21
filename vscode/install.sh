8f3b589f0b5c0ca9542c0961ce441771bc337965
Jeremy Felt <jeremy.felt@gmail.com>
2022-02-21 11:50:56 -0800

    Remove unused snippets link for VS Code

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,5 +1,4 @@
+# Remove the VS Code settings file and link a replacement
+# from this repo.
 rm $HOME/Library/Application\ Support/Code/User/settings.json
 ln -s $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-
-rm -rf $HOME/Library/Application\ Support/Code/User/snippets/
-ln -s $HOME/dotfiles/vscode/snippets/ $HOME/Library/Application\ Support/Code/User/snippets

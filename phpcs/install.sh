467eda3ae58f4f1a17c65d0fe9deda5b1c0ec443
jeremyfelt <jeremy.felt@gmail.com>
2024-04-18 08:59:11 -0700

    Remove global PHPCS configuration
    Fixes #48

--- phpcs/install.sh
+++
@@ -1,7 +0,0 @@
-# Check for an existing installation of Valet
-if test ! $(which phpcs)
-then
-  composer global require squizlabs/php_codesniffer --update-no-dev
-  composer global require dealerdirect/phpcodesniffer-composer-installer --update-no-dev
-  composer global require wp-coding-standards/wpcs --update-no-dev
-fi

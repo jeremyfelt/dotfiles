b0089e2b0669364c5cae2bc5f7ec4e94f942d193
jeremyfelt <jeremy.felt@gmail.com>
2026-01-07 16:03:50 -0800

    Update NVM 40.3

--- node/install.sh
+++ node/install.sh
@@ -2,11 +2,11 @@
 #
 # NVM: https://github.com/nvm-sh/nvm
 
-if test ! $(nvm --version | grep "39.1")
+if test ! $(nvm --version | grep "40.3")
 then
   rm -rf $HOME/.nvm
   mkdir $HOME/.nvm
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 fi
 
 export NVM_DIR=$HOME/.nvm
